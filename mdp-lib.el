b14e69f9e3bf586ca962474aaa61755261c670a4
Gonzalo Juarez <gonzalojuarezte@gmail.com>
2023-04-20 09:48:11 +0200

    Small refactor of 'action-state:prob->alist

--- mdp-lib.el
+++ mdp-lib.el
@@ -21,14 +21,12 @@
 (defun action-cost (state-action) (cadr state-action))
 (defun action-state:prob (state-action) (cddr state-action))
 
-(defun action-state:prob->alist (&rest action-state:prob*)
-  (-zip-pair (take-stepped action-state:prob* 2 0) ; some sliding-window algorithm would be better for this pattern
-	     (take-stepped action-state:prob* 2 1)))
-;; (action-dest->alist :u 0.5 :d 0.3 :e 0.2) translates to ((:u . 0.5) (:d . 0.3) (:e . 0.2))
+(defalias 'action-state:prob->alist (-partial #'-partition-in-steps 2 2))
+;; (action-state:prob->alist '(:u 0.5 :d 0.3 :e 0.2)) translates to '((:u 0.5) (:d 0.3) (:e 0.2))
 
 (defmacro ordered-action-probs (state-names state-action)
-  (let ((state:prob (apply #'action-state:prob->alist (action-state:prob state-action))))
-    `(list ,@(--map (alist-get it state:prob 0.0)
+  (let ((state:prob (action-state:prob->alist (action-state:prob state-action))))
+    `(list ,@(--map (car (alist-get it state:prob '(0.0))) ; default probability if not found in state:prob is 0.0
 		    state-names))))
 ;; (ordered-action-probs (:u :d :e) (:cost 10 :u 0.5 :e 0.2)) translates to (0.5 0.0 0.2)
 
@@ -77,12 +75,12 @@
 ;;; and the value is a list of actions indicating action :cost followed by probability of some or all states
 ;;; States with no actions (usually the final state) can have an empty list as their value (see :e below)
 
-(define-mdp mdp-ex-4
-  :u ((:cost 10 :u 0.5 :d 0.3 :e 0.2)
-      (:cost 25 :u 0.2 :d 0.7 :e 0.1))
-  :d ((:cost 10 :u 0.8 :e 0.2)
-      (:cost 25 :d 0.3 :e 0.7))
-  :e ())
+;; (define-mdp mdp-ex-4
+;;   :u ((:cost 10 :u 0.5 :d 0.3 :e 0.2)
+;;       (:cost 25 :u 0.2 :d 0.7 :e 0.1))
+;;   :d ((:cost 10 :u 0.8 :e 0.2)
+;;       (:cost 25 :d 0.3 :e 0.7))
+;;   :e ())
 
 ;;; This translates to roughly the following
 
@@ -95,7 +93,7 @@
 ;;    0))
 
 ;;; Solve the MDP
-(bellman-iterate #'mdp-ex-4 3)
+;; (bellman-iterate #'mdp-ex-4 3)
 
 ;;; Same result as the manually specified mdp
 ;; (bellman-iterate #'mdp-ex-4-expanded 3)
